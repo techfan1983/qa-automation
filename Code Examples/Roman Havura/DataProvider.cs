0d838e830b42eb12209e44ec8fc3fb336c5a3372
mehhrunes <roma7155@gmail.com>
2018-12-05 17:12:49 +0200

    Added sample code

---
+++ Code Examples/Roman Havura/DataProvider.cs
@@ -0,0 +1,25 @@
+﻿using Newtonsoft.Json.Linq;
+using NUnit.Framework;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaskNUnit.Utils
+{
+    class DataProvider : IEnumerable
+    {
+        private static readonly string calculationDataJson = 
+            File.ReadAllText($@"{TestContext.CurrentContext.WorkDirectory}\..\..\TestData\calculationData.json");
+        private static readonly JObject calculationData = JObject.Parse(calculationDataJson);
+
+        public IEnumerator GetEnumerator()
+        {
+            yield return (string)calculationData["firstCalculation"];
+            yield return (string)calculationData["secondCalculation"];
+        }
+    }
+}
