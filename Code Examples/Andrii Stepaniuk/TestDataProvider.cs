180bab246afc0e04007a5bf36caaf11e24e3580c
Andrii <stellaralnilam@gmail.com>
2018-12-05 14:21:20 -0600

    Added some code samples to the branch.

---
+++ Code Examples/Andrii Stepaniuk/TestDataProvider.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+
+namespace Stepaniuk.Homework.Tests
+{
+    class TestDataProvider : IEnumerable
+    {
+        public IEnumerator GetEnumerator()
+        {
+            yield return new string[] { "1", "John Smith", "admin@mail.com", "London", "Male" };
+            yield return new string[] { "2", "Jane Smith", "jane.smith@mail.com", "Tokio", "Female" };
+        }
+    }
+}
